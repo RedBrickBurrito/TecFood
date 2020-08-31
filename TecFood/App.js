7839c07cfe3893afb7b138d85a5917d74aded67f
Cesar Martinez <cesar.martinez0010@outlook.com>
2020-08-31 16:02:47 -0600

    Axios request to get info from database

--- TecFood/App.js
+++ TecFood/App.js
@@ -5,13 +5,27 @@ import {
   setStatusBarStyle,
 } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Platform, Button } from "react-native";
 import { useDeviceOrientation } from "@react-native-community/hooks";
+import axios from 'axios';
 
 export default function App() {
+  const getOrders = () => {
+    axios.get('https://tecfood.herokuapp.com/orders').then(response => {
+      console.log(response.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>POC Test IMOUTO!</Text>
+      <Button
+        onPress={getOrders}
+        title="Get Orders"
+        color="#f2e56f"
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
