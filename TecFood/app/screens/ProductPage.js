fdee1bfb9e8365ef4b211209c23b949fb85bd148
Cesar Martinez <cesar.martinez0010@outlook.com>
2020-09-26 14:08:42 -0600

    Added ProductService and created base ProductPage

---
+++ TecFood/app/screens/ProductPage.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { ImageBackground, StyleSheet, View, Text } from 'react-native'
+import { Card, CheckBox, Modal } from '@ui-kitten/components'
+import { 
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp 
+} from "react-native-responsive-screen";
+import { handleProductRequest } from '../../services/ProductService'
+
+
+function ProductPage({product_id, visible, hide}) {
+
+    const productData = handleProductRequest();
+
+    return (
+        <Modal 
+            visible={visible}
+            backdropStyle={styles.backdrop}
+            onBackdropPress={hide}
+        >
+            <Card style={styles.card}>
+                <ImageBackground style={styles.productImage}/>
+                <View style={styles.content}>
+                    <Text style={styles.attributesTitle}>Seleccionar Miel</Text>
+                    <View>
+                        <CheckBox>Miel de Agave</CheckBox>
+                    </View>
+                </View>
+            </Card>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    },
+    card: {
+        position: 'absolute',
+        alignSelf: 'center',
+        width: wp('85%'),
+        height: hp('61.3%'),
+        top: hp('22.3%'),
+        borderRadius: '29px',
+        backgroundColor: '#F7FBFB',
+    },
+    productImage: {
+        width: '100%',
+        height: '25%',
+        borderRadius: '29px',
+    },
+    content: {
+        padding: '10%'
+    },
+    attributesTitle: {
+        fontFamily: 'Open Sans',
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    attributes: {
+
+    }
+})
+
+export default ProductPage
---
+++ TecFood/services/ProductService.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export function handleProductRequest(id) {
+    //axios get request for product
+}
